d4df129f8424eb5429766bc5c664e4f588c681b6
ankitdhaked3811-beep <ankitdhaked3811@gmail.com>
2025-12-30 14:02:34 +0530

    Create layout.js

---
+++ app/app/api/app/layout.js
@@ -0,0 +1,10 @@
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <script src="https://cdn.tailwindcss.com"></script>
+      </head>
+      <body className="bg-slate-900">{children}</body>
+    </html>
+  );
+}
